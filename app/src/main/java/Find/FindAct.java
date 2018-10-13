0c77191baca0132a1ff74928fae92da03e72a206
Grain_Rain <h_liwei@qq.com>
2018-10-13 11:19:20 +0800

    First commit

---
+++ app/src/main/java/Find/FindAct.java
@@ -0,0 +1,17 @@
+package Find;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by 雨生百谷 on 2018/10/13.
+ */
+
+public class FindAct extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
